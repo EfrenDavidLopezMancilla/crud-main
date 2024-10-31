07fe77144bf476a20880da1b280d7e75d4e0d413
EfrenDavidLopezMancilla <101757727+EfrenDavidLopezMancilla@users.noreply.github.com>
2024-10-30 22:26:46 -0700

    gg

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  # Cambiado aquí
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
